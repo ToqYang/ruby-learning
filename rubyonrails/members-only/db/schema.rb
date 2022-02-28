201e8f9e2430c5a762e99ccbcef01dc3f74b7cfe
Santiago Yanguas <santitoya-2001@outlook.com>
2022-02-28 14:54:06 -0500

    [Feature] Add refs to users, updates, styles, partials, sessions

--- rubyonrails/members-only/db/schema.rb
+++ rubyonrails/members-only/db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_24_230907) do
+ActiveRecord::Schema.define(version: 2022_02_26_021634) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "body"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +35,5 @@ ActiveRecord::Schema.define(version: 2022_02_24_230907) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
