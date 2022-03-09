8efffeb34c7b1c270195706e3a225b1e4b648a18
Santiago Yanguas <santitoya-2001@outlook.com>
2022-03-09 08:02:43 -0500

    [Feature] Delegate function to app reflex

--- rubyonrails/simple-pm/app/reflexes/status_reflex.rb
+++ rubyonrails/simple-pm/app/reflexes/status_reflex.rb
@@ -34,7 +34,8 @@ class StatusReflex < ApplicationReflex
 
   def change
     task = Task.find(element.dataset[:id])
-    task.update(status: element.dataset[:status])
+    task.update(status: element.dataset[:status]) 
+    morph dom_id(task), render(TaskComponent.new(task: task))
   end
 
 end
