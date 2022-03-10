e9f430d678cfc5c3f435c1cd55e1f5d5cf351382
Santiago Yanguas <santitoya-2001@outlook.com>
2022-03-09 20:31:39 -0500

    [Feature] todo-list reflex, rails 7

---
+++ rubyonrails/todolist/app/javascript/controllers/check_controller.js
@@ -0,0 +1,31 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="check"
+export default class extends Controller {
+  connect() {
+    console.log('check controller connected');
+  }
+
+  active() {  
+    console.log('active');
+    console.log(event.target);
+    const id = event.target.dataset.id;
+    const csrfToken = document.querySelector('meta[name="csrf-token"]').content;
+
+    fetch(`/tasks/${id}/active`, {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': csrfToken
+      },
+      body: JSON.stringify({
+        completed: event.target.checked
+      })
+    })
+    .then((response) => response.text())
+    .then(Turbo.renderStreamMessage)
+  }
+}
