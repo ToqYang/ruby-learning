581dedda2a6726894f127c53fee9b9dbfcf3a9da
Santiago Yanguas <santitoya-2001@outlook.com>
2022-03-14 15:57:16 -0500

    [Feature] Seedfile

--- rubyonrails/odin-flight-booker/db/seeds.rb
+++ rubyonrails/odin-flight-booker/db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "STARR SEEDING"
+
+Airport.codes.values.each do |i|
+  if Airport.new(code: i).save
+    puts "Created airport #{i}"
+  else
+    puts "Failed to create airport #{i}"
+  end
+end
+
+MAX_AIRPORT_CODES = Airport.ids.max
+MIN_AIRPORT_CODES = Airport.ids.min
+
+100.times do |i|
+  departure = 0
+  arrival = 0
+
+  while true
+    departure = rand(MIN_AIRPORT_CODES..MAX_AIRPORT_CODES)
+    arrival = rand(MIN_AIRPORT_CODES..MAX_AIRPORT_CODES)
+
+    break if departure != arrival
+  end
+  
+  if Flight.new(departure_id: departure, arrival_id: arrival).save
+    puts "Created flight #{i}"
+  else
+    puts "Failed to create flight #{i}"
+  end
+end
